30375359402d279ca5a560cf5d326301fb72d13c
Sofia1-min <sofiahortunova@gmail.com>
2026-09-10 17:35:35 +0300

    Step 8: arithmetic and calculator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -31,7 +31,7 @@ Console.WriteLine(x - y);
 Console.WriteLine(x * y);
 Console.WriteLine(x / y);*/
 // Console.WriteLine(x % y);
-string firstName = "София";
+/*string firstName = "София";
 string lastName = "Хортюнова";
 string group = "ИСП-241";
 int birthYear = 2008;
@@ -47,4 +47,31 @@ Console.WriteLine($"Средний балл: {gpa}");
 Console.WriteLine($"Стипендия: {hasScholarship}");
 Console.Write("\nВведите ваш любимый предмет: ");
 string subject = Console.ReadLine();
-Console.WriteLine($"Отлично! {firstName} любит {subject}.");
+Console.WriteLine($"Отлично! {firstName} любит {subject}.");*/
+/*int a = 15;
+int b = 4;
+Console.WriteLine($"Сумма: {a + b}");
+Console.WriteLine($"Разность: {a - b}");
+Console.WriteLine($"Произведение: {a * b}");
+Console.WriteLine($"Частное (int): {a / b}");
+Console.WriteLine($"Остаток: {a % b}");
+double result = (double)a / b;
+Console.WriteLine($"Частное (double): {result}");
+Console.WriteLine(Math.Abs(-5));
+Console.WriteLine(Math.Pow(2, 10));
+Console.WriteLine(Math.Sqrt(144));
+Console.WriteLine(Math.Max(10, 25));
+Console.WriteLine(Math.Min(10, 25));
+Console.WriteLine(Math.Round(3.567, 2));*/
+Console.WriteLine("Калькулятор");
+Console.Write("Введите первое число: ");
+double num1 = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double num2 = double.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма: {num1 + num2}");
+Console.WriteLine($"Разность: {num1 - num2}");
+Console.WriteLine($"Произведение: {num1 * num2}");
+if (num2 != 0)
+    Console.WriteLine($"Частное: {num1 / num2}");
+else
+    Console.WriteLine("Деление на ноль невозможно!");
